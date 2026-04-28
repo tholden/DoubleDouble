ebd6e4a4bdbacad8e7c6b8c05b6fa426cfdf1d8a
Tom Holden <thomas.holden@gmail.com>
2026-04-28 17:28:27 +0200

    Create BackSubstitution.m

---
+++ private/BackSubstitution.m
@@ -0,0 +1,20 @@
+function v = BackSubstitution( v, U )
+    % For upper triangular U, x = BackSubstitution( b, U ) solves U*x = b.
+
+    [ m, n ] = size( U );
+    mn = min( m, n );
+    [ vm, vn ] = size( v );
+    if vm < n
+        v = [ v; zeros( n - vm, vn, 'like', v ) ];
+    elseif vm > n
+        v = v( 1 : n, : );
+    end
+
+    v( mn, : ) = v( mn, : ) ./ U( mn, mn );
+    for k = ( mn - 1 ) : -1 : 1
+        j = ( k + 1 ) : n;
+        t = sum( v( j, : ) .* U( k, j ).', 1 );
+        v( k, : ) = ( v( k, : ) - t ) ./ U( k, k );
+    end
+
+end
