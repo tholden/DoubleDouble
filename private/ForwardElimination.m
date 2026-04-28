ff3caad3af1eaef99a78355187d06275c7e8ac0f
Tom Holden <thomas.holden@gmail.com>
2026-04-28 17:28:29 +0200

    Create ForwardElimination.m

---
+++ private/ForwardElimination.m
@@ -0,0 +1,20 @@
+function v = ForwardElimination( v, L )
+    % For lower triangular L, x = ForwardElimination( b, L ) solves L*x = b.
+
+    [ m, n ] = size( L );
+    mn = min( m, n );
+    [ vm, vn ] = size( v );
+    if vm < n
+        v = [ v; zeros( n - vm, vn, 'like', v ) ];
+    elseif vm > n
+        v = v( 1 : n, : );
+    end
+
+    v( 1, : ) = v( 1, : ) ./ L( 1, 1 );
+    for k = 2 : mn
+        j = 1 : ( k - 1 );
+        t = sum( v( j, : ) .* L( k, j ).', 1 );
+        v( k, : ) = ( v( k, : ) - t ) ./ L( k, k );
+    end
+
+end
