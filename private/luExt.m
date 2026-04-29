cff0169e0bc03b46698b10f1c0a6d0b907c7c2ee
Tom Holden <thomas.holden@gmail.com>
2026-04-29 17:01:57 +0200

    Update luExt.m

--- private/luExt.m
+++ private/luExt.m
@@ -1,27 +1,52 @@
-function [ L, U, P ] = luExt( A )
-    % Type-agnostic LU decomposition with partial pivoting
+function [ v, U, p ] = luExt( v, type )
+    [ m, n ] = size( v );
+    p = 1 : m;
 
-    [ m, n ] = size( A );
-    P = eye( m );
-    L = eye( m, n, 'like', A );
-    U = A;
     for k = 1 : min( m, n )
-        [ ~, max_idx ] = max( abs( U( k : m, k ) ) );
-        max_idx = max_idx + k - 1;
 
-        if max_idx ~= k
-            U( [ k, max_idx ], : ) = U( [ max_idx, k ], : );
-            P( [ k, max_idx ], : ) = P( [ max_idx, k ], : );
-            if k > 1
-                L( [ k, max_idx ], 1 : ( k - 1 ) ) = L( [ max_idx, k ], 1 : ( k - 1 ) );
+        % Find index of largest element below diagonal in k-th column
+        [ ~, midx ] = max( abs( v( k:m, k ) ) );
+        midx = midx + k - 1;
+
+        % Skip elimination if column is zero
+        if v( midx, k ) ~= 0
+
+            % Swap pivot row
+            if midx ~= k
+                v( [ k midx ], : ) = v( [ midx k ], : );
+                p( [ k midx ] ) = p( [ midx k ] );
             end
-        end
 
-        if U( k, k ) ~= 0
-            idx = ( k + 1 ) : m;
-            L( idx, k ) = U( idx, k ) ./ U( k, k );
-            U( idx, k : n ) = U( idx, k : n ) - L( idx, k ) * U( k, k : n );
+            % Compute multipliers
+            i = ( k + 1 ) : m;
+            v( i, k ) = v( i, k ) ./ v( k, k );
+
+            % Update the remainder of the matrix
+            j = ( k + 1 ) : n;
+            v( i, j ) = v( i, j ) - v( i, k ) .* v( k, j );
         end
     end
 
+    if nargout > 1
+        % Separate result
+        L = tril( v, -1 ) + eye( m, n, 'like', v );
+        U = triu( v );
+        if n > m
+            L = L( :, 1:m );
+        elseif n < m
+            U = U( 1:n, : );
+        end
+        v = L;
+
+        if nargout > 2
+            if nargin < 2 || ~strcmp( type, 'vector' )
+                pp = eye( m );
+                pp = pp( p, : );
+                p = pp;
+            end
+        else
+            invp( p ) = 1 : m;
+            v = v( invp, : );
+        end
+    end
 end
