434dc7ec20f90dcc4ab22bcdc80e10f31e8b9252
Tom Holden <thomas.holden@gmail.com>
2026-05-04 18:11:25 +0200

    rename vars for comparison

--- +ED/private/UnderlyingDividedByDD.m
+++ +ED/private/UnderlyingDividedByDD.m
@@ -1,13 +1,13 @@
-function [ r1, r2 ] = UnderlyingDividedByDD( a, b1, b2 )
+function [ r1, r2 ] = UnderlyingDividedByDD( a1, b1, b2 )
     % Rescale to prevent overflow in intermediate products ( cf. QD library )
-    Rescale = abs( a ) > 2 ^ 969;
+    Rescale = abs( a1 ) > 2 ^ 969;
     if any( Rescale( : ) )
         ScaleDown = 2 ^ -53;
-        a( Rescale ) = a( Rescale ) * ScaleDown;
+        a1( Rescale ) = a1( Rescale ) * ScaleDown;
     end
-    q1 = a ./ b1;
+    q1 = a1 ./ b1;
     [ p1, p2 ] = DDTimesUnderlying( b1, b2, q1 );
-    [ r1, r2 ] = DDPlusUnderlying( -p1, -p2, a );
+    [ r1, r2 ] = DDPlusUnderlying( -p1, -p2, a1 );
     q2 = r1 ./ b1;
     [ p1, p2 ] = DDTimesUnderlying( b1, b2, q2 );
     [ r1, ~  ] = DDPlusDD( r1, r2, -p1, -p2 );
@@ -21,28 +21,28 @@ function [ r1, r2 ] = UnderlyingDividedByDD( a, b1, b2 )
     end
     Select = ( b1 == 0 ) & ( b2 == 0 );
     if any( Select( : ) )
-        if ( isscalar( Select ) ) && ( numel( a ) > 1 )
-            Select = repmat( Select, size( a ) );
-        elseif ( numel( Select ) > 1 ) && ( isscalar( a ) )
-            a = repmat( a, size( Select ) );
+        if ( isscalar( Select ) ) && ( numel( a1 ) > 1 )
+            Select = repmat( Select, size( a1 ) );
+        elseif ( numel( Select ) > 1 ) && ( isscalar( a1 ) )
+            a1 = repmat( a1, size( Select ) );
         end
-        aSelect = a( Select );
-        aSelect = sign( aSelect ) .* Inf;
-        r1( Select ) = aSelect;
-        r2( Select ) = aSelect;
+        a1Select = a1( Select );
+        a1Select = sign( a1Select ) .* Inf;
+        r1( Select ) = a1Select;
+        r2( Select ) = a1Select;
     end
     Select = isinf( b1 );
     if any( Select( : ) )
-        if ( isscalar( Select ) ) && ( numel( a ) > 1 )
-            Select = repmat( Select, size( a ) );
-        elseif ( numel( Select ) > 1 ) && ( isscalar( a ) )
-            a = repmat( a, size( Select ) );
+        if ( isscalar( Select ) ) && ( numel( a1 ) > 1 )
+            Select = repmat( Select, size( a1 ) );
+        elseif ( numel( Select ) > 1 ) && ( isscalar( a1 ) )
+            a1 = repmat( a1, size( Select ) );
         end
-        aSelect = a( Select );
-        aSelectSelect = ~isfinite( aSelect );
-        aSelect = 0;
-        aSelect( aSelectSelect ) = NaN;
-        r1( Select ) = aSelect;
-        r2( Select ) = aSelect;
+        a1Select = a1( Select );
+        a1SelectSelect = ~isfinite( a1Select );
+        a1Select = 0;
+        a1Select( a1SelectSelect ) = NaN;
+        r1( Select ) = a1Select;
+        r2( Select ) = a1Select;
     end
 end
