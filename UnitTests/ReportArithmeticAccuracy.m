c1f32a8d9c17651c1c1fffc1d5fbc5ddbe997219
Tom Holden <thomas.holden@gmail.com>
2026-05-07 17:28:50 +0200

    Update ReportArithmeticAccuracy.m

--- UnitTests/ReportArithmeticAccuracy.m
+++ UnitTests/ReportArithmeticAccuracy.m
@@ -18,9 +18,9 @@ fprintf('QuadDouble addition error: %g\n', max( rel ) );
 % Also test QuadDouble multiplication
 p = vpa_(a_ .* b_);
 p_vpa = a_vpa .* b_vpa;
-err_mul = abs(p - p_vpa);
-rel_mul = err_mul ./ max( 1, abs(p_vpa) );
-fprintf('QuadDouble multiplication error: %g\n', max( rel_mul ) );
+err = abs(p - p_vpa);
+rel = err ./ max( 1, abs(p_vpa) );
+fprintf('QuadDouble multiplication error: %g\n', max( rel ) );
 
 %% QuadDoubleSlow addition test
 % Construct two dense QuadDoubleSlow numbers with 8 doubles of significance
@@ -37,9 +37,9 @@ fprintf('QuadDoubleSlow addition error: %g\n', max( rel ) );
 % Also test QuadDoubleSlow multiplication
 p = vpa_(a_ .* b_);
 p_vpa = a_vpa .* b_vpa;
-err_mul = abs(p - p_vpa);
-rel_mul = err_mul ./ max( 1, abs(p_vpa) );
-fprintf('QuadDoubleSlow multiplication error: %g\n', max( rel_mul ) );
+err = abs(p - p_vpa);
+rel = err ./ max( 1, abs(p_vpa) );
+fprintf('QuadDoubleSlow multiplication error: %g\n', max( rel ) );
 
 %% OctoDouble addition test
 % Construct two dense OctoDouble numbers with 8 doubles of significance
@@ -56,9 +56,9 @@ fprintf('OctoDouble addition error: %g\n', max( rel ) );
 % Also test OctoDouble multiplication
 p = vpa_(a_ .* b_);
 p_vpa = a_vpa .* b_vpa;
-err_mul = abs(p - p_vpa);
-rel_mul = err_mul ./ max( 1, abs(p_vpa) );
-fprintf('OctoDouble multiplication error: %g\n', max( rel_mul ) );
+err = abs(p - p_vpa);
+rel = err ./ max( 1, abs(p_vpa) );
+fprintf('OctoDouble multiplication error: %g\n', max( rel ) );
 
 %% OctoDoubleAlt addition test
 % Construct two dense OctoDoubleAlt numbers with 8 doubles of significance
@@ -75,9 +75,9 @@ fprintf('OctoDoubleAlt addition error: %g\n', max( rel ) );
 % Also test OctoDoubleAlt multiplication
 p = vpa_(a_ .* b_);
 p_vpa = a_vpa .* b_vpa;
-err_mul = abs(p - p_vpa);
-rel_mul = err_mul ./ max( 1, abs(p_vpa) );
-fprintf('OctoDoubleAlt multiplication error: %g\n', max( rel_mul ) );
+err = abs(p - p_vpa);
+rel = err ./ max( 1, abs(p_vpa) );
+fprintf('OctoDoubleAlt multiplication error: %g\n', max( rel ) );
 
 %% OctoDoubleSlow addition test
 % Construct two dense OctoDoubleSlow numbers with 8 doubles of significance
@@ -94,7 +94,7 @@ fprintf('OctoDoubleSlow addition error: %g\n', max( rel ) );
 % Also test OctoDoubleSlow multiplication
 p = vpa_(a_ .* b_);
 p_vpa = a_vpa .* b_vpa;
-err_mul = abs(p - p_vpa);
-rel_mul = err_mul ./ max( 1, abs(p_vpa) );
-fprintf('OctoDoubleSlow multiplication error: %g\n', max( rel_mul ) );
+err = abs(p - p_vpa);
+rel = err ./ max( 1, abs(p_vpa) );
+fprintf('OctoDoubleSlow multiplication error: %g\n', max( rel ) );
 
