660ba9ace5a6505e7add5876c4be58d1b0fec8ef
Tom Holden <thomas.holden@gmail.com>
2026-05-07 20:31:14 +0200

    Partial Revert "IEEE QD addition"
    This reverts commit fa54dadb34f01b4601b26e6fbbff66c162af66e3.

--- +ED/private/QDPlusUnderlying.m
+++ +ED/private/QDPlusUnderlying.m
@@ -1,3 +1,7 @@
 function [ c0, c1, c2, c3 ] = QDPlusUnderlying( a0, a1, a2, a3, b )
-    [ c0, c1, c2, c3 ] = QDPlusQD( a0, a1, a2, a3, b, zeros( size( b ), 'like', b ), zeros( size( b ), 'like', b ), zeros( size( b ), 'like', b ) );
+    [ c0, e ] = UnderlyingPlusUnderlying( a0, b );
+    [ c1, e ] = UnderlyingPlusUnderlying( a1, e );
+    [ c2, e ] = UnderlyingPlusUnderlying( a2, e );
+    [ c3, e ] = UnderlyingPlusUnderlying( a3, e );
+    [ c0, c1, c2, c3 ] = Renorm5( c0, c1, c2, c3, e );
 end
