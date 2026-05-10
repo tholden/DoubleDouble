7e6c4520c3629294dcbae9123b8468444af35a16
Tom Holden <thomas.holden@gmail.com>
2026-05-10 13:05:32 +0200

    Update ReportArithmeticAccuracy.m

--- UnitTests/ReportArithmeticAccuracy.m
+++ UnitTests/ReportArithmeticAccuracy.m
@@ -5,10 +5,10 @@ rng( 'default' );
 a = OctoDouble.rand(100000,1);
 b = OctoDouble.rand(100000,1);
 
-Indices = unique([ 6956, 9046, 9606, 11026, 11125, 15313, 41086, 45919, 53079, 57708, 66528, 69459, 84018, 87435, 91179, 95916, 59508, 77345, 89690, 55764, 46399, 993, 24754, 4716, 33425, 93494, 94544, 6123 ]);
+% Indices = unique([ 6956, 9046, 9606, 11026, 11125, 15313, 41086, 45919, 53079, 57708, 66528, 69459, 84018, 87435, 91179, 95916, 59508, 77345, 89690, 55764, 46399, 993, 24754, 4716, 33425, 93494, 94544, 6123 ]);
 
-a = a(Indices);
-b = b(Indices);
+% a = a(Indices);
+% b = b(Indices);
 
 %% double addition test
 % Construct two dense double numbers
@@ -21,7 +21,8 @@ s_vpa = a_vpa + b_vpa;
 err = abs(s - s_vpa);
 rel = err ./ max( 1, abs(s_vpa) );
 max_rel = max( rel );
-fprintf('double         addition       error:\t%g\tIndices: %d\n', max_rel, find( rel.' == max_rel ) );
+idx = find( rel.' == max_rel, 1 );
+fprintf('double         addition       error:\t%g\tIndices: %d\n', max_rel, idx(1) );
 
 % Also test double multiplication
 p = vpa_(a_ .* b_);
@@ -29,7 +30,8 @@ p_vpa = a_vpa .* b_vpa;
 err = abs(p - p_vpa);
 rel = err ./ max( 1, abs(s_vpa) );
 max_rel = max( rel );
-fprintf('double         multiplication error:\t%g\tIndices: %d\n', max_rel, find( rel.' == max_rel ) );
+idx = find( rel.' == max_rel, 1 );
+fprintf('double         multiplication error:\t%g\tIndices: %d\n', max_rel, idx(1) );
 
 %% DoubleDouble addition test
 % Construct two dense DoubleDouble numbers
@@ -42,7 +44,8 @@ s_vpa = a_vpa + b_vpa;
 err = abs(s - s_vpa);
 rel = err ./ max( 1, abs(s_vpa) );
 max_rel = max( rel );
-fprintf('DoubleDouble   addition       error:\t%g\tIndices: %d\n', max_rel, find( rel.' == max_rel ) );
+idx = find( rel.' == max_rel, 1 );
+fprintf('DoubleDouble   addition       error:\t%g\tIndices: %d\n', max_rel, idx(1) );
 
 % Also test DoubleDouble multiplication
 p = vpa_(a_ .* b_);
@@ -50,7 +53,8 @@ p_vpa = a_vpa .* b_vpa;
 err = abs(p - p_vpa);
 rel = err ./ max( 1, abs(s_vpa) );
 max_rel = max( rel );
-fprintf('DoubleDouble   multiplication error:\t%g\tIndices: %d\n', max_rel, find( rel.' == max_rel ) );
+idx = find( rel.' == max_rel, 1 );
+fprintf('DoubleDouble   multiplication error:\t%g\tIndices: %d\n', max_rel, idx(1) );
 
 %% QuadDouble addition test
 % Construct two dense QuadDouble numbers
@@ -63,7 +67,8 @@ s_vpa = a_vpa + b_vpa;
 err = abs(s - s_vpa);
 rel = err ./ max( 1, abs(s_vpa) );
 max_rel = max( rel );
-fprintf('QuadDouble     addition       error:\t%g\tIndices: %d\n', max_rel, find( rel.' == max_rel ) );
+idx = find( rel.' == max_rel, 1 );
+fprintf('QuadDouble     addition       error:\t%g\tIndices: %d\n', max_rel, idx(1) );
 
 % Also test QuadDouble multiplication
 p = vpa_(a_ .* b_);
@@ -71,7 +76,8 @@ p_vpa = a_vpa .* b_vpa;
 err = abs(p - p_vpa);
 rel = err ./ max( 1, abs(s_vpa) );
 max_rel = max( rel );
-fprintf('QuadDouble     multiplication error:\t%g\tIndices: %d\n', max_rel, find( rel.' == max_rel ) );
+idx = find( rel.' == max_rel, 1 );
+fprintf('QuadDouble     multiplication error:\t%g\tIndices: %d\n', max_rel, idx(1) );
 
 %% QuadDoubleSlow addition test
 % Construct two dense QuadDoubleSlow numbers
@@ -84,7 +90,8 @@ s_vpa = a_vpa + b_vpa;
 err = abs(s - s_vpa);
 rel = err ./ max( 1, abs(s_vpa) );
 max_rel = max( rel );
-fprintf('QuadDoubleSlow addition       error:\t%g\tIndices: %d\n', max_rel, find( rel.' == max_rel ) );
+idx = find( rel.' == max_rel, 1 );
+fprintf('QuadDoubleSlow addition       error:\t%g\tIndices: %d\n', max_rel, idx(1) );
 
 % Also test QuadDoubleSlow multiplication
 p = vpa_(a_ .* b_);
@@ -92,7 +99,8 @@ p_vpa = a_vpa .* b_vpa;
 err = abs(p - p_vpa);
 rel = err ./ max( 1, abs(s_vpa) );
 max_rel = max( rel );
-fprintf('QuadDoubleSlow multiplication error:\t%g\tIndices: %d\n', max_rel, find( rel.' == max_rel ) );
+idx = find( rel.' == max_rel, 1 );
+fprintf('QuadDoubleSlow multiplication error:\t%g\tIndices: %d\n', max_rel, idx(1) );
 
 %% OctoDouble addition test
 % Construct two dense OctoDouble numbers
@@ -105,7 +113,8 @@ s_vpa = a_vpa + b_vpa;
 err = abs(s - s_vpa);
 rel = err ./ max( 1, abs(s_vpa) );
 max_rel = max( rel );
-fprintf('OctoDouble     addition       error:\t%g\tIndices: %d\n', max_rel, find( rel.' == max_rel ) );
+idx = find( rel.' == max_rel, 1 );
+fprintf('OctoDouble     addition       error:\t%g\tIndices: %d\n', max_rel, idx(1) );
 
 % Also test OctoDouble multiplication
 p = vpa_(a_ .* b_);
@@ -113,47 +122,6 @@ p_vpa = a_vpa .* b_vpa;
 err = abs(p - p_vpa);
 rel = err ./ max( 1, abs(s_vpa) );
 max_rel = max( rel );
-fprintf('OctoDouble     multiplication error:\t%g\tIndices: %d\n', max_rel, find( rel.' == max_rel ) );
-
-%% OctoDoubleAlt addition test
-% Construct two dense OctoDoubleAlt numbers
-a_ = OctoDoubleAlt( a );
-b_ = OctoDoubleAlt( b );
-a_vpa = vpa_(a_);
-b_vpa = vpa_(b_);
-s = vpa_(a_ + b_);
-s_vpa = a_vpa + b_vpa;
-err = abs(s - s_vpa);
-rel = err ./ max( 1, abs(s_vpa) );
-max_rel = max( rel );
-fprintf('OctoDoubleAlt  addition       error:\t%g\tIndices: %d\n', max_rel, find( rel.' == max_rel ) );
-
-% Also test OctoDoubleAlt multiplication
-p = vpa_(a_ .* b_);
-p_vpa = a_vpa .* b_vpa;
-err = abs(p - p_vpa);
-rel = err ./ max( 1, abs(s_vpa) );
-max_rel = max( rel );
-fprintf('OctoDoubleAlt  multiplication error:\t%g\tIndices: %d\n', max_rel, find( rel.' == max_rel ) );
-
-%% OctoDoubleSlow addition test
-% Construct two dense OctoDoubleSlow numbers
-a_ = OctoDoubleSlow( a );
-b_ = OctoDoubleSlow( b );
-a_vpa = vpa_(a_);
-b_vpa = vpa_(b_);
-s = vpa_(a_ + b_);
-s_vpa = a_vpa + b_vpa;
-err = abs(s - s_vpa);
-rel = err ./ max( 1, abs(s_vpa) );
-max_rel = max( rel );
-fprintf('OctoDoubleSlow addition       error:\t%g\tIndices: %d\n', max_rel, find( rel.' == max_rel ) );
-
-% Also test OctoDoubleSlow multiplication
-p = vpa_(a_ .* b_);
-p_vpa = a_vpa .* b_vpa;
-err = abs(p - p_vpa);
-rel = err ./ max( 1, abs(s_vpa) );
-max_rel = max( rel );
-fprintf('OctoDoubleSlow multiplication error:\t%g\tIndices: %d\n', max_rel, find( rel.' == max_rel ) );
+idx = find( rel.' == max_rel, 1 );
+fprintf('OctoDouble     multiplication error:\t%g\tIndices: %d\n', max_rel, idx(1) );
 
