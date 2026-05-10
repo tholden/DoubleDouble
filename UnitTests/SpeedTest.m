b4b136ddfeec113a5d04c9c2d50bf652350f240e
Tom Holden <thomas.holden@gmail.com>
2026-05-10 22:28:40 +0200

    Create SpeedTest.m

---
+++ UnitTests/SpeedTest.m
@@ -0,0 +1,12 @@
+a=QuadDoubleSlow.rand(200,200);
+b=QuadDoubleSlow.rand(200,200);
+tic;
+c1=a*b;
+toc;
+a=QuadDouble(a);
+b=QuadDouble(b);
+tic;
+c2=a*b;
+toc;
+c1=QuadDouble(c1);
+disp(max(abs(c1(:)-c2(:))));
